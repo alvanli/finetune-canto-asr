a7c382c765d82b1fdc621d30b241c084d6247b76
alvanli <51011489+alvanli@users.noreply.github.com>
2023-12-19 15:09:04 -0500

    some more cleaning

--- test.py
+++
@@ -1,7 +0,0 @@
-import transformers
-from datasets import load_dataset
-
-if __name__ == "__main__":
-    ds = load_dataset("mozilla-foundation/common_voice_8_0", "zh-HK", use_auth_token=True, streaming=True)
-    ds_1 = load_dataset("mozilla-foundation/common_voice_10_0", "zh-HK", split="train+validation", use_auth_token=True)
-    print(ds["train"].features)
