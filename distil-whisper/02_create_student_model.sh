6067f6fc23e5768d45aa1876940a7ebcd00c219f
Alvin @ sharkbyte <sharkbyte@uwdatascience.ca>
2024-04-04 21:17:49 -0400

    chng script

--- distil-whisper/02_create_student_model.sh
+++ distil-whisper/02_create_student_model.sh
@@ -3,5 +3,5 @@
 python3 create_student_model.py \
   --teacher_checkpoint "alvanlii/whisper-small-cantonese" \
   --encoder_layers 12 \
-  --decoder_layers 4 \
+  --decoder_layers 3 \
   --save_dir "./distil-small-init"
